e59b147f4124b1d01c667009fc63ebf2189904d4
Unknown <m.gaasbeek@ultraware.nl>
2019-08-27 11:35:42 +0200

    Remove i18n reference

--- generator/templates/src/pages/PageNotFound.vue
+++ generator/templates/src/pages/PageNotFound.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row justify-center">
     <div class="col-xs-12 col-md-8">
-      <h1>{{ $t('texts.pageNotFound.pageTitle') }}</h1>
+      <h1>Pagina niet gevonden</h1>
     </div>
   </div>
 </template>

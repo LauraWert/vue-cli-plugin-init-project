e2d6343e201fb6449a3dbbc5bee6be76fc2341b6
martijn788 <martijngaasbeek@gmail.com>
2019-08-27 11:56:06 +0200

    Fix footer (#8)

--- generator/templates/src/components/PageFooter.vue
+++ generator/templates/src/components/PageFooter.vue
@@ -3,18 +3,18 @@
     <div class="row footer justify-center">
       <div class="col-md-8 col-xs-12 row">
         <div class="col-md-2 row col-sm-3 col-xs-3 q-mt-xl">
-          {{ $t('texts.footer.placeholder') }}
+          placeholder
         </div>
         <div class="col-md-8 col-sm-9 col-xs-9">
           <div class="title">
-            {{ $t('texts.footer.placeholder') }}
+            placeholder
           </div>
           <div class="subtitle">
-            {{ $t('texts.footer.placeholder') }}
+            placeholder
           </div>
         </div>
         <div class="col-md-2 gt-sm q-mt-xl">
-          {{ $t('texts.footer.placeholder') }}
+          placeholder
         </div>
       </div>
     </div>
